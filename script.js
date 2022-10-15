6040b76517d18e9649621f51de3771834088f8cc
Eddie Dohyun Kim <eddiekimdohyun@gmail.com>
2022-10-15 00:00:23 -0400

    <a> tag symbol update

--- script.js
+++ script.js
@@ -48,9 +48,7 @@ function createJournal(x, n){
 	const notes = document.createElement('div');
 	notes.innerHTML = noteText;
 	notes.setAttribute('class', 'cell notes');
-	document.getElementById('row'+n).appendChild(notes);
-
-	
+	document.getElementById('row'+n).appendChild(notes);	
 }
 
 
@@ -59,15 +57,15 @@ function linkify(inputText) {
 
     //URLs starting with http://, https://, or ftp://
     replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a><span class="link">[&#10548;]</span>');
 
     //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
     replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2></a><span class="link">[&#10548;]</span>');
 
     //Change email addresses to mailto:: links.
     replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
-    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a><span class="link">[&#10548;]</span>');
 
     return replacedText;
 }
@@ -79,6 +77,5 @@ function findlink() {
 
 	var replace = linkify(htmlTxt);
 	html.innerHTML = replace;
-	
 }
 
